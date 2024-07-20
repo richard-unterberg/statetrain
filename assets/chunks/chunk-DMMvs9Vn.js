19c477c25b0dc18a5d316577e1a7428a188ec055
richard-unterberg <richard-unterberg@users.noreply.github.com>
2024-07-20 11:12:09 +0000

    deploy: 852e5be857b6002413cdb390ceb4bcb2cb138d3c

--- assets/chunks/chunk-DMMvs9Vn.js
+++ assets/chunks/chunk-DMMvs9Vn.js
@@ -1,4 +1,4 @@
-import{F as i,u as r,r as c,j as a,B as u,S as h,P as x}from"./chunk-DDs_QYll.js";const k=i.h4`
+import{F as i,u as r,r as c,j as a,B as u,S as h,P as x}from"./chunk-CAH7mpnC.js";const k=i.h4`
   text-2xl
   text-light
 `,m=({className:l,...n})=>{const{isPlaying:s,handlePlay:t,handleStop:e}=r(),o=c.useCallback(()=>{s?e():t()},[t,e,s]);return a.jsx(u,{className:`${s?"bg-warningDark":"bg-successDark animate-pulse"} ${l}`,onClick:o,icon:s?a.jsx(h,{className:"w-3 h-3",strokeWidth:4}):a.jsx(x,{className:"w-3 h-3",strokeWidth:4}),...n,children:s?"Stop":"Play"})};export{k as H,m as I};
