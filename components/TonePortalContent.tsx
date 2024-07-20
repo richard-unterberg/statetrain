778321d3d4812fd26f6a2d7e7f5a229855f1535f
Richard Unterberg <mail@richardunterberg.de>
2024-07-20 13:30:15 +0200

    🔍 minor seo changes

--- components/TonePortalContent.tsx
+++ components/TonePortalContent.tsx
@@ -1,15 +1,14 @@
 import Button from '#components/common/Button'
-import H2Headline from '#components/common/H2Headline'
 import { APP_CONFIG } from '#lib/constants'
 
 const TonePortalContent = ({ handleClick }: { handleClick: () => void }) => (
-  <div className="flex justify-center">
+  <section className="flex justify-center">
     <div className="mt-16 p-3">
       <div className="text-5xl text-center mb-5">🚂🚃</div>
-      <H2Headline className="text-4xl text-center font-black mb-6">STATETRAIN</H2Headline>
-      <p className="text-xl md:w-3/4 mx-auto mb-10 text-center text-light">
+      <h1 className="text-4xl text-center font-black mb-6">STATETRAIN</h1>
+      <h2 className="text-xl md:w-3/4 mx-auto mb-6 text-center text-light">
         {APP_CONFIG.description}
-      </p>
+      </h2>
       <Button
         className="bg-successDark text-white w-full mb-3 max-w-60 justify-center mx-auto"
         onClick={handleClick}
@@ -20,7 +19,7 @@ const TonePortalContent = ({ handleClick }: { handleClick: () => void }) => (
         This button unlocks tone.js context for all following pages / components
       </p>
     </div>
-  </div>
+  </section>
 )
 
 export default TonePortalContent
